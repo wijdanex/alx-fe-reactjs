171546e930d33e0f9e39c8478e41318b5b65052a
Wijdane <wijdanemellouz@gmail.com>
2025-10-31 15:04:57 +0100

    Add 3 components

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,12 +1,16 @@
-import './App.css'
-import WelcomeMessage from './components/WelcomeMessage';
+import './App.css';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <>
-      <WelcomeMessage />
+      <Header />
+      <MainContent />
+      <Footer />
     </>
-  )
+  );
 }
 
 export default App;
