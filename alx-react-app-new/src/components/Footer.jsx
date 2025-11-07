42e661b677294ab96e1afd3666b8d79b8e6882b1
Wijdane <wijdanemellouz@gmail.com>
2025-11-07 21:56:34 +0100

    Initial commit with new project

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'black', color: 'white', textAlign: 'center', padding: '10px', marginTop: '20px' }}>
+      <p>&copy; 2025 My City Guide</p>
+    </footer>
+  );
+}
+
+export default Footer;
